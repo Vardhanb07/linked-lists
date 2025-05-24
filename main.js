2773b2d5114356c9b6945a09b3846a89ee4baee3
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-05-24 11:47:08 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import { linkedList } from "./linkedList.js"
+const list = new linkedList()
+list.append("dog")
+list.append("cat")
+list.append("parrot")
+list.append("hamster")
+list.append("snake")
+list.append("turtle")
+console.log(list.toString())
